42d3029b7bb1b955800fcd1aebce6dc519fb81b5
Utkarsh-1771 <utkarsh1765uv7@gmail.com>
2026-08-23 04:11:18 +0530

    added bird movements and start button functionality

--- script.js
+++ script.js
@@ -5,10 +5,23 @@ const bird=document.querySelector(".bird");
 const scoreDisplay=document.querySelector("#score");
 const start=document.querySelector("#start-button");
 const restart=document.querySelector("#restart-button");
+bird.style.top=birdPosition+"px";
 start.addEventListener("click", function()
 {
     start.style.display="none";
     isRunning=true;
     scoreDisplay.textContent="0";
-    console.log(isRunning);
 });
+document.addEventListener("keydown",function(event)
+{
+    if(event.code==="Space" && isRunning)
+    {
+        event.preventDefault();
+        birdPosition=birdPosition-40;
+        bird.style.top=birdPosition+"px";
+    }
+});
+setInterval(() => {
+   birdPosition=birdPosition+2;
+   bird.style.top=birdPosition+"px";
+}, 20);
